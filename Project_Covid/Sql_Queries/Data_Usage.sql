3dd4eaecac61ba3942e6fd574daf53037210f26e
L.Pol <123669377+LPintos14@users.noreply.github.com>
2024-05-27 19:54:43 -0300

    Update and rename Project_Covid/Data_Usage.sql to Project_Covid/Sql_Queries/Data_Usage.sql

--- Project_Covid/Sql_Queries/Data_Usage.sql
+++ Project_Covid/Sql_Queries/Data_Usage.sql
@@ -1,12 +1,12 @@
 
-# Deaths Percentage  
+-- Deaths Percentage  
 select location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as Death_Percentage
 from data 
 where location not like 'Africa'and location not like 'asia' and location not like "europe" and location not like "%income%"
 and location not like "%european" and location not like "%world" and location not like "%america%" 
 and total_cases is not NULL  order by 1,2;
-
-# Total deaths per continent
+ 
+-- Total deaths per continent
 select continent , max(total_deaths) as total_deaths 
 from data 
 where location not like 'Africa'and location not like 'asia' and location not like "europe" and location not like "%income%"
@@ -14,7 +14,7 @@ and location not like "%european" and location not like "%world" and location no
 group by continent 
 order by 1;
 
-# Infected Population per date
+-- Infected Population per date
 select location,population, date, max(total_cases) as Max_cases, MAX((total_cases/population*100)) as InfectedPopulation_Percentage
 from data
 where location not like 'Africa'and location not like 'asia' and location not like "europe" and location not like "%income%"
@@ -23,7 +23,7 @@ group by location,population,`date`
 order by 4 desc;
 
 
-#Infected Population
+-- Infected Population
 select location,population, max(total_cases) as Max_cases, MAX((total_cases/population*100)) as InfectedPopulation_Percentage
 from data
 where location not like 'Africa'and location not like 'asia' and location not like "europe" and location not like "%income%"
